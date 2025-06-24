79513b9e6aabc8d61331890ba452c457ec23ede7
Cali0310 <ramosyazmien2@gmail.com>
2025-06-25 00:09:38 +0800

    added file paths

--- face_mask_detector_oop_final_project.py
+++ face_mask_detector_oop_final_project.py
@@ -2,3 +2,6 @@ import cv2
 import tensorflow as tf
 import keras
 import numpy as np
+
+haar_cascade_file_path = r"C:\\Users\\dell\\PycharmProjects\\PythonProject\\.venv\\Lib\\haarcascade_frontalface_default.xml"
+model_file_path = r"C:\\Users\\dell\\PycharmProjects\\PythonProject\\.venv\\Lib\\mask_recog.h5"
