728f9e99d02ab0f8306b468add2caef8651f5b3d
Cali0310 <ramosyazmien2@gmail.com>
2025-06-25 00:11:45 +0800

    added label-to-text mapping for mask detection

--- face_mask_detector_oop_final_project.py
+++ face_mask_detector_oop_final_project.py
@@ -5,3 +5,6 @@ import numpy as np
 
 haar_cascade_file_path = r"C:\\Users\\dell\\PycharmProjects\\PythonProject\\.venv\\Lib\\haarcascade_frontalface_default.xml"
 model_file_path = r"C:\\Users\\dell\\PycharmProjects\\PythonProject\\.venv\\Lib\\mask_recog.h5"
+
+label_text_mapping = {0: 'without_mask', 1: 'with_mask'}
+bounding_box_color_mapping = {0: (0, 0, 255), 1: (0, 255, 0)}
