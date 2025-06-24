18ada259b23576d5d05006e8514546621c7585de
Cali0310 <ramosyazmien2@gmail.com>
2025-06-25 00:34:12 +0800

    initialize with mask detection system

--- video_frame_processor.py
+++ video_frame_processor.py
@@ -5,3 +5,5 @@ label_text_mapping = {0: 'without_mask', 1: 'with_mask'}
 bounding_box_color_mapping = {0: (0, 0, 255), 1: (0, 255, 0)}
 
 class VideoFrameProcessor:
+    def __init__(self, mask_detection_system: MaskDetectionSystem):
+        self.mask_detection_system = mask_detection_system
