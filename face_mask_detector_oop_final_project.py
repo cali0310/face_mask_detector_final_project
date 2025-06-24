5f416eb578b0d3252c88d548fdd234e722623fee
Cali0310 <ramosyazmien2@gmail.com>
2025-06-25 00:57:02 +0800

    add main script to initialize and run

--- face_mask_detector_oop_final_project.py
+++ face_mask_detector_oop_final_project.py
@@ -1,10 +1,13 @@
-import cv2
-import tensorflow as tf
-import keras
-import numpy as np
+from mask_detection_system import MaskDetectionSystem
+from video_frame_processor import VideoFrameProcessor
+from webcam_video_stream import WebcamVideoStream
 
+# Constants
 haar_cascade_file_path = r"C:\\Users\\dell\\PycharmProjects\\PythonProject\\.venv\\Lib\\haarcascade_frontalface_default.xml"
 model_file_path = r"C:\\Users\\dell\\PycharmProjects\\PythonProject\\.venv\\Lib\\mask_recog.h5"
 
-label_text_mapping = {0: 'without_mask', 1: 'with_mask'}
-bounding_box_color_mapping = {0: (0, 0, 255), 1: (0, 255, 0)}
+if __name__ == '__main__':
+    mask_system = MaskDetectionSystem(model_file_path, haar_cascade_file_path)
+    frame_processor = VideoFrameProcessor(mask_system)
+    webcam_stream = WebcamVideoStream(frame_processor)
+    webcam_stream.run_stream()
