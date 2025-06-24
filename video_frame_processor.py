cfff40bdc58884c71036dd0fc9cab5829b2b3458
Cali0310 <ramosyazmien2@gmail.com>
2025-06-25 00:33:20 +0800

    add label and color for labels

--- video_frame_processor.py
+++ video_frame_processor.py
@@ -1,2 +1,7 @@
 import cv2
 from mask_detection_system import MaskDetectionSystem
+
+label_text_mapping = {0: 'without_mask', 1: 'with_mask'}
+bounding_box_color_mapping = {0: (0, 0, 255), 1: (0, 255, 0)}
+
+class VideoFrameProcessor:
